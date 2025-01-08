77c8393df62fcc379b962fea9f18170b8f370c2a
KpFormics <khushbu@formics.io>
2025-01-08 16:22:56 +0530

    footer UI improved

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,15 +1,22 @@
 <section class=" py-[70px] px-[5%]">
   <div class="max-w-7xl mx-auto py-[70px] md:px-28 px-10 shadow-[0px_4px_20px_0px_#5A463524] rounded-2xl	">
     <!-- Top Section -->
-    <div class="flex flex-col md:flex-row md:items-center justify-between gap-4">
+    <div class="flex flex-col lg:flex-row items-center justify-between gap-4 ">
       <!-- Logo Section -->
       <div class="flex items-center space-x-2">
-        <img src="/logo.png" alt="MZ Design Cons." class="h-8 w-8" />
-        <span class="text-[#0056D6] font-bold text-lg">MZ Design Cons.</span>
+        <!-- <img src="/logo.png" alt="MZ Design Cons." class="h-8 w-8" /> -->
+        <svg width="51" height="49" viewBox="0 0 51 49" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M25.5366 46.2683C37.9832 46.2683 48.0732 36.1783 48.0732 23.7317C48.0732 11.2851 37.9832 1.19507 25.5366 1.19507C13.09 1.19507 3 11.2851 3 23.7317C3 36.1783 13.09 46.2683 25.5366 46.2683Z" fill="#003EFF"/>
+          <path d="M33.2493 26.0354L35.5969 26.9161L30.4998 29.7935" fill="white"/>
+          <path d="M25.8961 23.7748L28.8961 22.4648L22.0989 32.1956V34.9656L24.5912 36.6368L25.2948 36.2211L35.6423 30.4954L24.6491 36.4136V33.3591L33.1227 20.0542L29.3919 21.9077L29.38 21.9128L29.2556 21.969L29.2232 21.9844" fill="white"/>
+          <path d="M36.5215 17.3059C36.4908 16.5478 36.116 15.972 35.5453 15.6534H35.5487L35.5283 15.6432C35.4499 15.6006 35.0871 15.4558 35.0019 15.4234L24.6237 10.8271L14.484 16.144L14.7225 30.1185L17.0717 31.6568L16.8281 17.3757L24.4073 13.2173L25.7719 13.8817L18.1927 18.0622L18.4687 32.575L20.9576 34.2258L20.7038 19.3229L28.283 15.1457L29.668 15.8357L22.0888 20.0214L22.1007 23.3893L24.651 24.5835V20.9719L32.1757 16.989L32.5402 16.8067L34.254 16.0503C34.7191 15.8902 35.1348 15.9515 35.392 16.0231C35.869 16.2496 36.2216 16.7147 36.237 17.3229C36.2574 18.1116 36.2796 18.6551 36.2915 19.0946C36.2915 19.0946 36.2915 19.098 36.2915 19.0997C36.329 20.3416 35.9814 21.5665 35.2915 22.6006L34.0002 24.6329L35.3647 22.7334C36.2148 21.604 36.6458 20.2139 36.5862 18.8016C36.5641 18.2599 36.5385 17.7334 36.5215 17.3093V17.3059Z" fill="white"/>
+          </svg>
+          
+        <span class="text-[#0056D6] font-bold text-2xl">MZ Design Cons.</span>
       </div>
 
       <!-- Navigation Links -->
-      <nav class="flex gap-8 text-center  flex-wrap text-base">
+      <nav class="flex gap-6	 text-center  flex-wrap text-lg	 flex-col sm:flex-row	">
         <a href="/" class="hover:underline">Home</a>
         <a href="/service" class="hover:underline">Service</a>
         <a href="/portfolio" class="hover:underline">Portfolio</a>
@@ -23,16 +30,16 @@
       <!-- Social Media Icons First on Mobile -->
       <div class="flex space-x-4  order-1 md:order-2">
         <a href="/" class="hover:text-gray-700">
-          <img src="/icons/twitter-fill.svg" alt="Twitter" class="size-4" />
+          <img src="/icons/twitter-fill.svg" alt="Twitter" class="size-8" />
         </a>
         <a href="/" class="hover:text-gray-700">
-          <img src="/icons/Vector.svg" alt="Instagram" class="size-4" />
+          <img src="/icons/Vector.svg" alt="Instagram" class="size-7" />
         </a>
         <a href="/" class="hover:text-gray-700">
-          <img src="/icons/Vector (1).svg" alt="LinkedIn" class="size-4" />
+          <img src="/icons/Vector (1).svg" alt="LinkedIn" class="size-7" />
         </a>
         <a href="/" class="hover:text-gray-700">
-          <img src="/icons/Vector (2).svg" alt="WhatsApp" class="size-4" />
+          <img src="/icons/Vector (2).svg" alt="WhatsApp" class="size-7" />
         </a>
       </div>
       <!-- Copyright Text Second on Mobile -->

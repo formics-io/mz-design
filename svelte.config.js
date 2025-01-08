20460475b9726505c72e0b9b5d16be63305a63a5
KpFormics <khushbu@formics.io>
2025-01-08 15:35:20 +0530

    added netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
 // svelte.config.js
 import vercel from '@sveltejs/adapter-vercel';
+import netlify from '@sveltejs/adapter-netlify';
+
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -7,7 +9,9 @@ const config = {
     preprocess: vitePreprocess(),
 
     kit: {
-        adapter: vercel(),
+        // adapter: vercel(),
+        adapter: netlify(),
+
         // Optionally, specify other configurations here
     }
 };

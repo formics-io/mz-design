d769a27b0ae04f21cd9fe4bfe1a7e51ff07d6f27
KpFormics <khushbu@formics.io>
2025-02-20 11:47:58 +0530

    add whatsapp detail and all contact live detail

--- src/lib/components/Contactus.svelte
+++ src/lib/components/Contactus.svelte
@@ -2,24 +2,31 @@
     	const contacts = [
 		{
 			id: 1,
-			icon: '/icons/contactMZ/location.svg', 
+			icon: '/icons/contactMZ/Email.svg', 
 			title: 'Email',
-			detail: 'support@gulaly.com',
+			detail: 'admin@mz02.com',
 			buttonLabel: 'CONTACT'
 		},
 		{
 			id: 2,
 			icon: '/icons/contactMZ/Phone.svg', 
 			title: 'Phone',
-			detail: 'Office. +61 234–567 8910',
+			detail: 'Office. 03-61862822' ,
 			buttonLabel: 'CALL'
 		},
 		{
 			id: 3,
 			icon: '/icons/contactMZ/location.svg', 
 			title: 'Office',
-			detail: '6391 Elgin St. Celina, Delaware 10299',
+			detail: '57A, Jalan SG 3/1, Pusat Bandar Sri Gombak, 68100 Batu Caves Selangor, Malaysia',
 			buttonLabel: 'LOCATION'
+		},
+		{
+			id: 4,
+			icon: '/icons/contactMZ/WHATSAPP.svg', 
+			title: 'Whatsapp',
+			detail: '+601158671745',
+			buttonLabel: 'CONTACT'
 		}
 	];
 </script>
@@ -33,32 +40,33 @@
                 feedback, we’re here to help.
             </p>
         </div>
-		<div class="flex flex-wrap flex-row gap-4 mx-auto justify-center">
+		<div class="grid md:grid-cols-2 2xl:grid-cols-4 gap-4 mx-auto justify-center ">
 			{#each contacts as contact}
-				<div class=" md:w-[400px]  w-full  rounded-xl border  p-6 text-left shadow-md">
-					<div class="">
-						<img class="flex-shrink-0 text-2xl " 
-							src={contact.icon}
-                            alt={contact.title}
-						/>
-						<h3 class="text-lg mt-4 font-semibold m ">{contact.title}</h3>
-					</div>
-					<p class="mb-4 font-general-sans text-sm font-semibold leading-6 text-left  underline-offset-[from-font] decoration-skip-ink-none">
+			<div class=" w-full h-full rounded-xl border p-6 text-left shadow-md flex flex-col">
+				<img class="flex-shrink-0 text-2xl max-w-[24px]" src={contact.icon} alt={contact.title} />
+			
+				<div class="flex-grow">
+					<h3 class="text-lg mt-4 font-semibold">{contact.title}</h3>
+					<p class="mb-4 font-general-sans text-sm font-semibold leading-6 text-left underline-offset-[from-font] decoration-skip-ink-none">
 						{#if contact.title === 'Phone'}
 							<span>Office. </span><span class="underline">+61 234–567 8910</span>
 						{:else}
 							{contact.detail}
 						{/if}
-					</p>									 
-					<div class="flex gap-2 ">
-                        <button 
-						class="text-xs bg-[#003EFF]  text-white	font-semibold  w-[93px] h-[32px] rounded-full  px-[16px] py-[8px] text-center text-sm  transition hover:bg-white hover:text-black hover:ring-1 hover:ring-[#003EFF]"
+					</p>    
+				</div>
+			
+				<div class="flex gap-2 mt-auto">
+					<button 
+						class="text-xs bg-[#003EFF] text-white font-semibold w-[93px] h-[32px] rounded-full px-[16px] py-[8px] text-center text-sm transition hover:bg-white hover:text-black hover:ring-1 hover:ring-[#003EFF]"
 					>
+					
 						{contact.buttonLabel}
 					</button>
 					<p class="mt-2 text-xs text-gray-500">*extra information(optional)</p>
-                    </div>
 				</div>
+			</div>
+			
 			{/each}
 		</div>
 	</div>
